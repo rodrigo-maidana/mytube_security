fc8da00b6b011051d1a6cf58e812a720a6aebfc2
Rodrigo Maidana <ro-maidana@hotmail.com>
2024-11-18 18:24:51 -0300

    disable cors

--- src/main/java/com/fiuni/mytube_security/config/CorsConfig.java
+++ src/main/java/com/fiuni/mytube_security/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.fiuni.mytube_security.config;
+/*package com.fiuni.mytube_security.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -17,4 +17,4 @@ public class CorsConfig implements WebMvcConfigurer {
                 .allowedHeaders("*")
                 .allowCredentials(false);
     }
-}
+}*/
